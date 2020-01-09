6975c2f625d7f8fe79a77e6ac64de538db8cc10d
Taylor Hutchison <seanthutchison@gmail.com>
2020-01-09 09:33:13 -0600

    Testing variable shadowing.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,6 @@
+#[allow(unused_variables)]
 fn main() {
     let x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let x = 6;
+    println!("The value of x is {}", x);
 }
